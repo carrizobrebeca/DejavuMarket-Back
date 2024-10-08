3fd2c4882ae00299bad5224b51f41427536892ba
carrizobrebeca <carrizob.rebeca@gmail.com>
2024-10-08 14:42:46 -0300

    correccion handler

--- api/src/Handlers/usersHandler.js
+++ api/src/Handlers/usersHandler.js
@@ -9,7 +9,7 @@ const postUsersHandler = async (req, res) => {
   const { nombre, apellido, email, password, imagen } = req.body;
   
   try {
-    const response = await postProductos(nombre, apellido, email, password, imagen );
+    const response = await postUsuarios(nombre, apellido, email, password, imagen );
     res.status(200).json(response);
  } catch (error) {
     res.status(400).json({error: error.message});
