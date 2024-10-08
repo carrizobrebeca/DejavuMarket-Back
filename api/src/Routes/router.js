cd71740488094934ad97cc43232ff71d3f848e34
carrizobrebeca <carrizob.rebeca@gmail.com>
2024-10-08 14:55:09 -0300

    eliminar handler

--- api/src/Routes/router.js
+++ api/src/Routes/router.js
@@ -1,6 +1,6 @@
 const {Router} = require('express');
 const productsRouter = require('./productsRouter');
-const usersRouter = require('./usersRouter');
+// const usersRouter = require('./usersRouter');
 const carritoRouter = require('./carritoRouter');
 const pagoRouter = require('./pagoRouter');
 const preferenceRouter = require('./preferenceRouter');
@@ -8,7 +8,7 @@ const preferenceRouter = require('./preferenceRouter');
 const router = Router();
 
 router.use("/products", productsRouter);
-router.use("/users", usersRouter);
+// router.use("/users", usersRouter);
 router.use("/carrito", carritoRouter);
 router.use("/pago", pagoRouter);
 router.use("/create_preference", preferenceRouter)
